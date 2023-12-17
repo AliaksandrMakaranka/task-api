18ac2c8cfff69f1416ca200be99c130359d06148
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2023-12-17 02:04:28 +0100

    feat: add DB postgress

--- src/main/java/com/skynet/taskapi/TaskApiApplication.java
+++ src/main/java/com/skynet/taskapi/TaskApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TaskApiApplication.class, args);
 	}
 
