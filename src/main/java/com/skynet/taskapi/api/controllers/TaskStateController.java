3f2f008b185f7d3be97657b7389e7dbcee609e88
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2023-12-21 00:54:17 +0100

    feat: TSC add update task method

--- src/main/java/com/skynet/taskapi/api/controllers/TaskStateController.java
+++ src/main/java/com/skynet/taskapi/api/controllers/TaskStateController.java
@@ -4,6 +4,7 @@ package com.skynet.taskapi.api.controllers;
 import com.skynet.taskapi.api.controllers.helpers.ControllerHelper;
 import com.skynet.taskapi.api.dto.TaskStateDto;
 import com.skynet.taskapi.api.exceptions.BadRequestException;
+import com.skynet.taskapi.api.exceptions.NotFoundException;
 import com.skynet.taskapi.api.factories.TaskStateDtoFactory;
 import com.skynet.taskapi.store.entities.ProjectEntity;
 import com.skynet.taskapi.store.entities.TaskStateEntity;
@@ -32,7 +33,7 @@ public class TaskStateController {
 
     public static final String GET_TASK_STATES = "/api/projects/{project_id}/task-states";
     public static final String CREATE_TASK_STATE = "/api/projects/{project_id}/task-states";
-    public static final String DELETE_PROJECT = "/api/projects/{project_id}";
+    public static final String UPDATE_TASK_STATE = "/api/task-states/{task_state_id}";
 
 
     @GetMapping(GET_TASK_STATES)
@@ -58,18 +59,20 @@ public class TaskStateController {
 
         ProjectEntity project = controllerHelper.getProjectOrThrowException(projectId);
 
-        project
-                .getTaskStates()
-                .stream()
-                .map(TaskStateEntity::getName)
-                .filter(anotherTaskStateName -> anotherTaskStateName.equalsIgnoreCase(taskStateName))
-                .findAny()
-                .ifPresent(it -> {
-                    throw new BadRequestException(String.format("Task state \"%s\" already exist.", taskStateName));
-                });
+        Optional<TaskStateEntity> optionalAnotherTaskState = Optional.empty();
 
-        Optional<TaskStateEntity> optionalAnotherTaskState = taskStateRepository
-                .findTaskStateEntityByRightTaskStateIdIsNullAndProjectId(projectId);
+        for (TaskStateEntity taskState : project.getTaskStates()) {
+
+            if (taskState.getName().equalsIgnoreCase(taskStateName)) {
+                throw new BadRequestException(String.format("Task state \"%s\" already exist.", taskStateName));
+            }
+
+            if (!taskState.getRightTaskState().isPresent()) {
+                optionalAnotherTaskState = Optional.of(taskState);
+                break;
+            }
+
+        }
 
         TaskStateEntity taskState = taskStateRepository.saveAndFlush(
                 TaskStateEntity.builder()
@@ -91,6 +94,47 @@ public class TaskStateController {
         final TaskStateEntity savedTaskState = taskStateRepository.saveAndFlush(taskState);
 
         return taskStateDtoFactory.makeTaskStateDto(savedTaskState);
+    }
+
+    @PostMapping(UPDATE_TASK_STATE)
+    public TaskStateDto updateTaskState(@PathVariable(name = "task_state_id") Long taskStateId,
+                                        @RequestParam(name = "task_state_name") String taskStateName) {
+
+        if (taskStateName.trim().isEmpty()) {
+            throw new BadRequestException("Task state name can't be empty");
+        }
+
+        TaskStateEntity taskState = getTaskStateOrThrowException(taskStateId);
+
+        taskStateRepository
+                .findTaskStateEntityByProjectIdAndNameContainsIgnoreCase(
+                        taskState.getProject().getId(),
+                        taskStateName
+                )
+                .filter(anotherTaskState -> !anotherTaskState.getId().equals(taskStateId))
+                .ifPresent(anotherTaskState -> {
+                    throw new BadRequestException(String.format("Task state \"%s\" already exist.", taskStateName));
+                });
+
+        taskState.setName(taskStateName);
+
+        taskState = taskStateRepository.saveAndFlush(taskState);
+
+        return taskStateDtoFactory.makeTaskStateDto(taskState);
+    }
+
+    private TaskStateEntity getTaskStateOrThrowException(Long taskStateId) {
+
+        return taskStateRepository
+                .findById(taskStateId)
+                .orElseThrow(() ->
+                        new NotFoundException(
+                                String.format(
+                                        "Task state with \"%s\" id doesn't exist.",
+                                        taskStateId
+                                )
+                        )
+                );
     }
 
 }
--- src/main/java/com/skynet/taskapi/store/repositories/TaskStateRepository.java
+++ src/main/java/com/skynet/taskapi/store/repositories/TaskStateRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TaskStateRepository extends JpaRepository<TaskStateEntity, Long> {
-    Optional<TaskStateEntity> findTaskStateEntityByRightTaskStateIdIsNullAndProjectId(Long projectId);
+
+    Optional<TaskStateEntity> findTaskStateEntityByProjectIdAndNameContainsIgnoreCase(Long projectId, String taskStateName);
 }
